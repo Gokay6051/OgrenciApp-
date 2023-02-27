9e680eabbcb9d5111cff3cd36e3aedfa3d335d7c
Ahmet Gökay Ürkmez <gokayurkmez_541@hotmail.com>
2023-02-27 23:40:33 +0300

    Messages page edited.

--- lib/mesajlar_sayfasi.dart
+++ lib/mesajlar_sayfasi.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MesajlarSayfasi extends StatefulWidget {
@@ -14,17 +16,67 @@ class _MesajlarSayfasiState extends State<MesajlarSayfasi> {
       appBar: AppBar(
         title: Text("Mesajlar"),
       ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        bool benMiyim = true;
-        return Align(
-          alignment: Alignment.centerRight,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.black, width: 1)),
-            child: Text("Mesaj"),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                reverse: true,
+                itemBuilder: (context, index) {
+                  bool benMiyim = Random().nextBool();
+                  return Align(
+                    alignment:
+                        benMiyim ? Alignment.centerRight : Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 1,
+                            ),
+                            color: benMiyim ? Colors.lightGreen : Colors.cyan,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 7, horizontal: 8),
+                          child: Text(
+                            "Mesaj uzun mesaj",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
           ),
-        );
-      }),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        color: Colors.white30,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                      )),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child:
+                    ElevatedButton(onPressed: () {}, child: Icon(Icons.send)),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
